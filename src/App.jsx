eff935caa9004bfed23fbb050c83c1ce3a278d37
Sofi Belocchi <sofiabelocchiet@gmail.com>
2024-05-02 18:25:33 -0300

    arreglos en el css store

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Register from "./component/register/Register";
 import Navbar from "./component/navbar/Navbar";
 import { UserProvider } from "./context/UserContext";
 import { Route,Routes, BrowserRouter } from "react-router-dom"
+import Store from "./component/store/Store";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
         <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<h1>home</h1>} />
-          <Route path="/store" element={<h1>store</h1>} />
+          <Route path="/store" element={<Store></Store>} />
           <Route path="/contact" element={<h1>contact</h1>} />
           <Route path="/team" element={<h1>team</h1>} />
           <Route path="/login" element={<Login></Login>} />
