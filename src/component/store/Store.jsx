831dff76e8d39c211a3c5f0d5ef2f5ef7a27982a
Sofi Belocchi <sofiabelocchiet@gmail.com>
2024-06-27 20:09:08 -0300

    New Filter in Store

--- src/component/store/Store.jsx
+++ src/component/store/Store.jsx
@@ -6,14 +6,18 @@ import { UserContext } from "../../context/UserContext";
 import { getAll } from "../../service/functionsHTTP";
 
 const URL = "http://localhost:3000/api/products";
+const URlCategories = "http://localhost:3000/api/categories";
 
 const PRODUCTS_PER_PAGE = 15;
 
 const Store = () => {
+
   const { user } = useContext(UserContext);
 
   const [products, setProducts] = useState([]);
-  const navigate = useNavigate();
+
+  const [categories, setCategories] = useState([]);
+
   const [searchTerm, setSearchTerm] = useState("");
 
   const [minPrice, setMinPrice] = useState("");
@@ -22,32 +26,43 @@ const Store = () => {
   const [isPriceRangeOpen, setIsPriceRangeOpen] = useState(false);
 
   const [isConditionOpen, setIsConditionOpen] = useState(false);
-  const [conditionFilter, setConditionFilter] = useState("");  
+  const [conditionFilter, setConditionFilter] = useState("");
 
+  const [categoryFilter, setCategoryFilter] = useState(null);
   const [isCategoryOpen, setIsCategoryOpen] = useState(false);
-  const [categoryFilter, setCategoryFilter] = useState("");
 
   const [currentPage, setCurrentPage] = useState(1);
+  
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (user) { getProducts() };
-  },
-    [currentPage, user]); // Se vuelve a cargar cuando cambia la página
+    if (user) {
+      getProducts();
+      getCategories();
+    }
+  }, [user]);
 
-    
+  const getCategories = async () => {
+    try {
+      const res = await fetch(URlCategories, {
+        method: "GET",
+      });
+      const result = await res.json();
+      setCategories(result);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
   const getProducts = async () => {
     try {
-      const res = await getAll(URL, user.jwt)
-      
+      const res = await getAll(URL, user.jwt);
       if (!res.ok) {
-        throw new Error('Failed to fetch products');
+        throw new Error("Failed to fetch products");
       }
-
       const result = await res.json();
       setProducts(result);
-
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error fetching products:", error);
     }
   };
@@ -74,34 +89,27 @@ const Store = () => {
 
   const handleConditionFilter = (condition) => {
     setConditionFilter(condition);
-    setCurrentPage(1); // Reiniciar la página al cambiar el filtro
-  };
-
-  const handleCategoryFilter = (category) => {
-    setCategoryFilter(category);
     setCurrentPage(1);
   };
 
-  // Filtrar los productos según los filtros seleccionados
+  const handleCategoryFilter = (category) => {
+    setCategoryFilter(category ? category.id : null);
+    setCurrentPage(1);
+  };
+
   const filteredProducts = products.filter((product) => {
     const nameMatch = product.product_name.toLowerCase().includes(searchTerm.toLowerCase());
     const priceMatch =
       (!minPrice || product.price >= parseFloat(minPrice)) &&
       (!maxPrice || product.price <= parseFloat(maxPrice));
     const conditionMatch = !conditionFilter || product.condition === conditionFilter;
-    const categoryMatch = !categoryFilter || product.category === categoryFilter;
-
+    const categoryMatch = !categoryFilter || product.category_id === categoryFilter;
     return nameMatch && priceMatch && conditionMatch && categoryMatch;
   });
 
-  // Calcula la cantidad de paginas segun el número total de productos
   const pageCount = Math.ceil(filteredProducts.length / PRODUCTS_PER_PAGE);
-
-  // Calcula el índice inicial y final de los productos en función de la página actual
   const startIndex = (currentPage - 1) * PRODUCTS_PER_PAGE;
   const endIndex = startIndex + PRODUCTS_PER_PAGE;
-
-  // Filtra los productos y muestra solo los que están en el rango de índices calculado
   const productsToShow = filteredProducts.slice(startIndex, endIndex);
 
   const countByCondition = (condition) => {
@@ -110,15 +118,12 @@ const Store = () => {
 
   const changePage = (page) => {
     setCurrentPage(page);
-    window.scrollTo(0, 0); // Scroll hasta la parte superior de la página
+    window.scrollTo(0, 0);
   };
-
 
   const handleLinkClick = (productId) => {
     navigate(`/product/${productId}`);
   };
-
-
 
   return (
     <div className="shop">
@@ -181,7 +186,7 @@ const Store = () => {
                       className={conditionFilter === "USED" ? "active" : ""}
                       onClick={() => handleConditionFilter("USED")}
                     >
-                     Usado ({countByCondition("USED")})
+                      Usado ({countByCondition("USED")})
                     </button>
                   </div>
                 )}
@@ -193,93 +198,33 @@ const Store = () => {
                 {isCategoryOpen && (
                   <div className="categoryFilterContent">
                     <button
-                      className={categoryFilter === "" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("")}
+                      className={categoryFilter === null ? "active" : ""}
+                      onClick={() => handleCategoryFilter(null)}
                     >
-                      Todas
+                      Todos los productos
                     </button>
-                    <button
-                      className={categoryFilter === "ELECTRONICS" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("ELECTRONICS")}
-                    >
-                      Electrónica
-                    </button>
-                    <button
-                      className={categoryFilter === "CLOTHES" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("CLOTHES")}
-                    >
-                      Indumentaria
-                    </button>
-                    <button
-                      className={categoryFilter === "ACCESORIES" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("ACCESORIES")}
-                    >
-                      Accesorios
-                    </button>
-                    <button
-                      className={categoryFilter === "AUTOMOTIVE" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("AUTOMOTIVE")}
-                    >
-                      Automotriz
-                    </button>
-                    <button
-                      className={categoryFilter === "PETS" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("PETS")}
-                    >
-                      Mascotas
-                    </button>
-                    <button
-                      className={categoryFilter === "HOME" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("HOME")}
-                    >
-                      Hogar y cocina
-                    </button>
-                    <button
-                      className={categoryFilter === "HEALTH" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("HEALTH")}
-                    >
-                      Salud y belleza
-                    </button>
-                    <button
-                      className={categoryFilter === "SPORTS" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("SPORTS")}
-                    >
-                      Deportes y aire libre
-                    </button>
-                    <button
-                      className={categoryFilter === "GAMES" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("GAMES")}
-                    >
-                      Juguetes y juegos
-                    </button>
-                    <button
-                      className={categoryFilter === "BOOKSMUSIC" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("BOOKSMUSIC")}
-                    >
-                      Libros y musica
-                    </button>
-                    <button
-                      className={categoryFilter === "FOOD" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("FOOD")}
-                    >
-                      Alimentos y comida
-                    </button>
-                    <button
-                      className={categoryFilter === "ART" ? "active" : ""}
-                      onClick={() => handleCategoryFilter("ART")}
-                    >
-                      Arte y decoracion
-                    </button>
-                   
+                    {categories.map((category) => (
+                      <button
+                        key={category.id}
+                        className={categoryFilter === category.id ? "active" : ""}
+                        onClick={() => handleCategoryFilter(category)}
+                      >
+                        {category.name}
+                      </button>
+                    ))}
                   </div>
                 )}
               </div>
             </div>
-           </div>
-           <div className="productsContainer">
+          </div>
+          <div className="productsContainer">
             <div className="products">
               {productsToShow.map((product) => (
-                <ProductCard key={product.product_name} handleLinkClickProduct={() => handleLinkClick(product.productId)} data={product} />
+                <ProductCard
+                  key={product.productId}
+                  handleLinkClickProduct={() => handleLinkClick(product.productId)}
+                  data={product}
+                />
               ))}
             </div>
             {pageCount > 1 && (
@@ -297,7 +242,6 @@ const Store = () => {
             )}
           </div>
         </div>
-
       </div>
     </div>
   );
