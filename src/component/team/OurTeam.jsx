557718c21dcf8de673c286640310bd8a74651cef
FranciscogDiaz <franciscogdiaz12@gmail.com>
2024-07-01 23:45:34 -0300

    linkedinadded

--- src/component/team/OurTeam.jsx
+++ src/component/team/OurTeam.jsx
@@ -1,10 +1,10 @@
 import "./team.css";
-import Team from "../../assets/team.png";
 import Fran from "../../assets/francisco.svg";
 import Mati from "../../assets/mati.svg";
 import Matu from "../../assets/matu.svg";
 import Sofia from "../../assets/sofia.svg";
 import Github from "../../assets/mdi_github.png";
+import Linkedin from "../../assets/linkedin_icon.svg";
 import { Link } from "react-router-dom";
 const Ourteam = () => {
   const members = [
@@ -12,21 +12,28 @@ const Ourteam = () => {
       img: Fran,
       name: "Francisco Diaz",
       Github: "https://github.com/FranciscogDiaz",
+      Linkedin: "https://www.linkedin.com/in/francisco-diaz1/",
     },
     {
       img: Mati,
       name: "Matias Cagliero",
       Github: "https://github.com/MatiasCagliero-95",
+      Linkedin:
+        "https://ar.linkedin.com/in/matias-cagliero-6585921b4?trk=public_profile_browsemap-profile",
     },
     {
       img: Matu,
       name: "Matias-F-Acevedo",
       Github: "https://github.com/Matias-F-Acevedo",
+      Linkedin:
+        "https://www.linkedin.com/in/matías-acevedo?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAEMegEgB-VpzGrcQ2pCtuIr7xF3mB733b00&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BG2rPTiO%2FSGi0tWcMVhHP6A%3D%3D}",
     },
     {
       img: Sofia,
       name: "Sofia Belocchi",
       Github: "https://github.com/BelocchiSofia1",
+      Linkedin:
+        "https://ar.linkedin.com/in/sofia-belocchi-b06009273?trk=public_profile_browsemap-profile",
     },
   ];
   return (
@@ -34,8 +41,8 @@ const Ourteam = () => {
       <section className="team-title">
         <h2 className="title">Nuestro equipo</h2>
         <p className="subtitle">
-          Siguenos en Github para enterarte de las novedades y herramientas
-          que añadimos en la plataforma.
+          Siguenos en Github para enterarte de las novedades y herramientas que
+          añadimos en la plataforma.
         </p>
       </section>
       <section className="member-wrapper">
@@ -43,9 +50,14 @@ const Ourteam = () => {
           <article className="member-card" key={index}>
             <img src={member.img} alt="team-members" />
             <h3>{member.name}</h3>
-            <Link to={member.Github} target="_blank">
-              <img src={Github} alt="github-icon" />
-            </Link>
+            <div>
+              <Link to={member.Github} target="_blank">
+                <img src={Github} alt="github-icon" />
+              </Link>
+              <Link to={member.Linkedin} target="_blank">
+                <img src={Linkedin} alt="github-icon" />
+              </Link>
+            </div>
           </article>
         ))}
       </section>
